93410b427161a374a6498f86461f3e28e15cecc3
Murray Williams <murray.williams@ventureweb.com>
2018-02-11 21:12:38 +0200

    Created timer form and rendered it inside timer dashboard component

--- src/components/TimerDashboard.js
+++ src/components/TimerDashboard.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
+// Components
+import TimerForm from './TimerForm';
+
 class TimerDashboard extends Component {
   render() {
     return (
       <div className="container-fluid">
-
+        <TimerForm />
       </div>
     )
   }
---
+++ src/components/TimerForm.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class TimerForm extends Component {
+  render() {
+    return (
+      <div className="form-row">
+        <div className="col-7">
+          <input type="text" className="form-control" placeholder="What are you working on?" />
+        </div>
+        <div className="col">
+          <select class="custom-select" id="projectSelect">
+            <option selected>Select a project...</option>
+            <option value="project1">Project 1</option>
+            <option value="project2">Project 2</option>
+            <option value="project3">Project 3</option>
+          </select>
+        </div>
+        <div className="col-auto">
+        <button type="submit" class="btn btn-success mb-2">Add Entry</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default TimerForm;
+
