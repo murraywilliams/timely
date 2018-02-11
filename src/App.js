eed18f54b692ce5f63fa578517c8ccf908fd061e
Murray Williams <murray.williams@ventureweb.com>
2018-02-11 21:09:33 +0200

    Created timerdashboard component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// Components
+import TimerDashboard from './components/TimerDashboard';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <h1 className="display-4 text-left">Add a new time entry</h1>
+        <TimerDashboard />
       </div>
     );
   }
--- src/components/TimerDashboard.js
+++ src/components/TimerDashboard.js
@@ -1,2 +1,13 @@
 import React, { Component } from 'react';
 
+class TimerDashboard extends Component {
+  render() {
+    return (
+      <div className="container-fluid">
+
+      </div>
+    )
+  }
+}
+
+export default TimerDashboard;
